77d544c99f89f1c8a36071ba04b5976feb3a78c6
Sagar <42873729+SagarCodeCtrl@users.noreply.github.com>
2024-03-14 18:01:38 +0530

    add var2.sh

---
+++ variables/var2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+host="Windows"
+os="Rocky Linux"
+version=9
+
+echo "Hello there, this is $os running on $host"
+
+echo "Hello There, now its $os"
+
+echo "The version of $os is $version"
