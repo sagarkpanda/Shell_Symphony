ee6687c2e543d92173c4166952747f6214169d6c
Sagar <42873729+SagarCodeCtrl@users.noreply.github.com>
2024-03-16 17:06:37 +0530

    Update maths.sh

--- math_function/maths.sh
+++ math_function/maths.sh
@@ -5,7 +5,7 @@ second=3
 
 echo "The third value is: $(expr $first + $second)"
 echo "The multiplied value is: $(expr $first \* $second)"
-echo "$first devided by $second is: $(expr $first / $second)"
-echo "$first devided by $second is: $(echo "scale=1; $first / $second" | bc -l)"
+echo "$first divided by $second is: $(expr $first / $second)"
+echo "$first divided by $second is: $(echo "scale=1; $first / $second" | bc -l)"
 
 # the * in linux has a special use, which refers to all directories,so for multiplication use \ as an escape sequence so the bash understands to use this a multiplication symbol.
