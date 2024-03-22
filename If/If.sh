08753523d24e839c2eaf5785251b1bf252c0b240
Sagar <42873729+SagarCodeCtrl@users.noreply.github.com>
2024-03-22 18:50:05 +0530

    Add If.sh

---
+++ If/If.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+a=5
+
+if [ $a -eq 5 ]
+then
+    echo "The condition is True"
+fi
+
+#-----------------------------------#
+
+if test $a -eq 5
+then
+    echo "The condition is True"
+fi
