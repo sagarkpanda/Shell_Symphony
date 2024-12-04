f55faede18de74ac523280e52f07ffb0865b1e5e
Sagar <42873729+sagarkpanda@users.noreply.github.com>
2024-12-04 20:49:38 +0530

    Update case statement

--- Loops/case.sh
+++ Loops/case.sh
@@ -11,3 +11,29 @@ fi
 echo You chose $script
 done
 echo Bye
+
+#-----
+
+#!/bin/bash
+
+PS3="Please enter your choice: "  # Custom prompt text
+
+select option in "List Files" "Show Date" "Exit"
+do
+    case $option in
+        "List Files")
+            echo "Listing files in the current directory:"
+            ls
+            ;;
+        "Show Date")
+            echo "Current date and time: $(date)"
+            ;;
+        "Exit")
+            echo "Goodbye!"
+            break  # Exit the loop and end the script
+            ;;
+        *)
+            echo "Invalid option, please try again."
+            ;;
+    esac
+done
